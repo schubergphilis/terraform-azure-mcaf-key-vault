0f57ed87c6b43de30f63a8ba7df2c62075b02dbf
Ferry Blankendaal <fblankendaal@schubergphilis.com>
2025-01-15 12:06:38 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -128,7 +128,7 @@ DESCRIPTION
 
 variable "role_assignments" {
   type = map(object({
-    role_definition_id_or_name             = string
+    role_definition_name                   = string
     principal_id                           = string
     description                            = optional(string, null)
     skip_service_principal_aad_check       = optional(bool, false)
