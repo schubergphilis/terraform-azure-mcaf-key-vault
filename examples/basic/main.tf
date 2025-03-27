aa49aca2fd66bf5286aa970989367f136cc4f890
Niek van Raaij <nvanraaij@schubergphilis.com>
2025-03-27 15:00:20 +0100

    chore: slim down basic example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -15,17 +15,7 @@ module "key_vault" {
   name                            = "my-key-vault"
   tenant_id                       = "e55dda83-e842-4de8-9889-2903a7ebaf15"
   resource_group_name             = "resource-group-name"
-  enabled_for_disk_encryption     = true
-  public_network_access_enabled   = false
-  enabled_for_deployment          = false
-  enabled_for_template_deployment = false
-  enable_rbac_authorization       = true
-  purge_protection                = true
-  default_network_action          = "Deny"
-  soft_delete_retention_days      = 30
-  sku                             = "standard"
-  ip_rules                        = []
-  subnet_ids                      = []
+  
   network_bypass                  = "AzureServices"
   cmkrsa_key_name                 = "cmkrsa"
   cmk_keys_create                 = true
